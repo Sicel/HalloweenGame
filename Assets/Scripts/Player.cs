c76c6757b4cfdc8db78ab3080cb37529b85d87d0
cxc9597 <cxc9597@IGM2025-06>
2019-10-02 18:35:55 -0400

    Starting prototype

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [HideInInspector]
+    Rigidbody2D rigidbody;
+
+    [SerializeField]
+    float speed = 20;
+
+    [SerializeField]
+    float jumpForce = 100;
+
+    bool onGround = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        rigidbody.velocity = new Vector2(horizontal * speed, rigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W))
+            rigidbody.AddForce(new Vector2(0, jumpForce));
+    }
+}
