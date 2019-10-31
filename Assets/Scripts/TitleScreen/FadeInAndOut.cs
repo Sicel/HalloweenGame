2d8e65abc6f95c77623d902b8cd1096605e91054
Edenhype <edenhype@gmail.com>
2019-10-31 01:24:08 -0400

    titlescreen, bgm

---
+++ Assets/Scripts/TitleScreen/FadeInAndOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInAndOut : MonoBehaviour
+{
+    public Text myText;
+    public Color fromColor;
+    public Color toColor;
+
+    float timer = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime * 2;
+
+        myText.color = Color.Lerp(fromColor, toColor, Mathf.Sin(timer * 0.5f) + 0.5f);
+    }
+}
---
+++ Assets/Scripts/TitleScreen/VolumeControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    public Slider slider;
+    public AudioSource audioSource;
+
+    public float multiplier = 1;
+
+    void Start() {
+        float vol = PlayerPrefs.GetFloat("volume", 1);
+        PlayerPrefs.SetFloat("volume", vol);
+
+        if (slider != null)
+            slider.value = PlayerPrefs.GetFloat("volume");
+
+        if (audioSource != null)
+            audioSource.volume = PlayerPrefs.GetFloat("volume") * multiplier;
+    }
+
+    public void SetVolume(float value) {
+        PlayerPrefs.SetFloat("volume", value);
+    }
+
+    void Update() {
+        if (audioSource != null)
+            audioSource.volume = PlayerPrefs.GetFloat("volume") * multiplier;
+    }
+
+    void onApplicationQuit() {
+        PlayerPrefs.Save();
+    }
+}
